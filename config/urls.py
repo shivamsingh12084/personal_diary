8ed7b6f5951e1f90cd5aaeab95c8f2746d81fda9
shivamsingh12084 <shivamsingh12084@gmail.com>
2021-09-17 22:21:33 +0530

    creation of authentication

--- config/urls.py
+++ config/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include # new
+from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')), # new
+
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local apps
+    path('accounts/', include('accounts.urls')), # new
+    path('', include('pages.urls')),
 ]
